b686307b7b701157c24469f23543f706d5d517a3
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-18 01:58:27 +0000

    collect weight from user add conversion display user

--- .ccls-cache/@home@runner@lab3-1/main.cpp
+++ .ccls-cache/@home@runner@lab3-1/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
+int main() 
+{
+  double pounds, kilograms;
+  const double CONVERSION_FACTOR = 2.20462;
+  
+  //ask user to input weight
+  cout << "Enter weight in pounds" << endl;
+  cin >> pounds;
+  
+  //convert weight to kilograms
+  kilograms = pounds/CONVERSION_FACTOR;
 
-int main() {
-  std::cout << "Hello World!\n";
+  cout << "Your weight in kilograms is " << kilograms << endl;
+  return 0;
 }
